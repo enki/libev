4d217453a90d0ad5e9df9e6ca6b53df62f6444e7
Grant Henke <granthenke@users.noreply.github.com>
2020-11-23 19:30:51 -0600

    Update ev++.h

--- ev++.h
+++ ev++.h
@@ -376,7 +376,7 @@ namespace ev {
 
   struct default_loop : loop_ref
   {
-    default_loop (unsigned int flags = AUTO) throw noexcept(false)
+    default_loop (unsigned int flags = AUTO) noexcept(false)
 #if EV_MULTIPLICITY
     : loop_ref (ev_default_loop (flags))
 #endif
